4f95ca053e1b4835da89915b1a9b29fed6ee6df9
Yurii Lisovenko <yriyliso@gmail.com>
2021-07-05 20:38:14 +0300

    added item template

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import { Link, Switch, Route, Redirect } from 'react-router-dom';
+import { useState } from 'react';
+import classNames from 'classnames';
+import { LoginForm } from './LoginForm';
+import { Products } from './Products';
+import { CardItem } from './CardItem';
 
 function App() {
+  const [isLogin, setIsLogin] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      <header className={classNames("header", isLogin && "header--loged")}>
+        <div className="header__login-page login-page">
+          <div className="login-page__logo">
+            <Link to="/">
+              Логотип
+            </Link>
+          </div>
+
+          {isLogin ? (
+            <div className="login-page__sign-in">
+              <Link to="/products">
+                Продукция
+              </Link>
+            </div>
+          ) : (
+            <div className="login-page__sign-in">
+              <Link to="/login-form">
+                Войти в аккаунт
+              </Link>
+            </div>
+          )}
+        </div>
+
+        {isLogin && (
+          <div className="logout">
+            <Link to="/" onClick={() => setIsLogin(false)}>
+              Выйти из аккаунта
+            </Link>
+          </div>
+        )}
+
       </header>
+
+      <main className="main">
+        <Switch>
+          <Route path="/login-form">
+            <LoginForm onLogin={setIsLogin} isLogin={isLogin} />
+          </Route>
+
+          <Route path="/products">
+            <Products isLogin={isLogin} />
+          </Route>
+
+          <Route path="/cardItem">
+            <CardItem isLogin={isLogin} />
+          </Route>
+        </Switch>
+
+      </main>
+
+      <footer className="footer">
+      </footer>
     </div>
   );
 }
